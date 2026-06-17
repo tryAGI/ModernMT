73f3f82265df533dc39b5c43f58e0d6d2cb13b80
Konstantin S. <havendv@gmail.com>
2026-06-17 19:35:56 +0400

    feat: Updated OpenAPI spec (#29)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/cli/ModernMT.CLI/CliRuntime.cs
+++ src/cli/ModernMT.CLI/CliRuntime.cs
@@ -24,14 +24,12 @@ internal static class CliRuntime
             ? new global::System.Collections.Generic.List<global::ModernMT.EndPointAuthorization>()
             : new global::System.Collections.Generic.List<global::ModernMT.EndPointAuthorization>
             {
-        new global::ModernMT.EndPointAuthorization
-        {
-            Type = "ApiKey",
-            SchemeId = "ApikeyMmtApikey",
-            Location = "Header",
-            Name = "MMT-ApiKey",
-            Value = apiKey,
-        },
+        CreateAuthorization(
+            type: "ApiKey",
+            schemeId: "ApikeyMmtApikey",
+            location: "Header",
+            name: "MMT-ApiKey",
+            value: apiKey),
             };
         var baseUri = ResolveBaseUri(parseResult);
 
@@ -40,6 +38,30 @@ internal static class CliRuntime
             baseUri: baseUri,
             authorizations: authorizations,
             disposeHttpClient: true);
+    }
+
+    private static global::ModernMT.EndPointAuthorization CreateAuthorization(
+        string type,
+        string schemeId,
+        string location,
+        string name,
+        string value)
+    {
+        var authorization = new global::ModernMT.EndPointAuthorization
+        {
+            Type = type,
+            Location = location,
+            Name = name,
+            Value = value,
+        };
+
+        var schemeIdProperty = typeof(global::ModernMT.EndPointAuthorization).GetProperty("SchemeId");
+        if (schemeIdProperty?.CanWrite == true)
+        {
+            schemeIdProperty.SetValue(authorization, schemeId);
+        }
+
+        return authorization;
     }
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage(
